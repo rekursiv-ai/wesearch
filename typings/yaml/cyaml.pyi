780ac80465049dbcf01f1c4be26e82655869c865
Joshua V. Dillon <1137078+jvdillon@users.noreply.github.com>
2026-09-15 06:10:11 +0000

    Export custom YAML type stubs for safe_load overload.
    Wesearch export branch: wesearch/export/main
    copybarista-source-rev-sha256=ee1cd0c8726ce4432a6958e651465aa4b993d9d1e6c8039d1d2095a5691d7084
    copybarista-replay-base-sha256=c22ab686a9ca2973e35bcaefcbeaa22dce059435c3908ff3c00f5af719df8b0e
    copybarista-pr-state-version=1

---
+++ typings/yaml/cyaml.pyi
@@ -0,0 +1,85 @@
+from collections.abc import Mapping, Sequence
+from typing import IO, Any
+
+from _typeshed import SupportsRead
+
+from ._yaml import CEmitter, CParser
+from .constructor import (
+    BaseConstructor,
+    FullConstructor,
+    SafeConstructor,
+    UnsafeConstructor,
+)
+from .representer import BaseRepresenter, SafeRepresenter
+from .resolver import BaseResolver, Resolver
+
+__all__ = [
+    "CBaseDumper",
+    "CBaseLoader",
+    "CDumper",
+    "CFullLoader",
+    "CLoader",
+    "CSafeDumper",
+    "CSafeLoader",
+    "CUnsafeLoader",
+]
+
+type _Readable = SupportsRead[str | bytes]
+type _CLoader = (
+    CLoader | CBaseLoader | CFullLoader | CSafeLoader | CUnsafeLoader
+)  # Used in other modules
+
+class CBaseLoader(CParser, BaseConstructor, BaseResolver):
+    def __init__(self, stream: str | bytes | _Readable) -> None: ...
+
+class CLoader(CParser, SafeConstructor, Resolver):
+    def __init__(self, stream: str | bytes | _Readable) -> None: ...
+
+class CSafeLoader(CParser, SafeConstructor, Resolver):
+    def __init__(self, stream: str | bytes | _Readable) -> None: ...
+
+class CFullLoader(CParser, FullConstructor, Resolver):
+    def __init__(self, stream: str | bytes | _Readable) -> None: ...
+
+class CUnsafeLoader(CParser, UnsafeConstructor, Resolver):
+    def __init__(self, stream: str | bytes | _Readable) -> None: ...
+
+class CBaseDumper(CEmitter, BaseRepresenter, BaseResolver):
+    def __init__(
+        self,
+        stream: IO[Any],
+        default_style: str | None = None,
+        default_flow_style: bool | None = False,
+        canonical=None,
+        indent: int | None = None,
+        width: int | None = None,
+        allow_unicode=None,
+        line_break: str | None = None,
+        encoding: str | None = None,
+        explicit_start=None,
+        explicit_end=None,
+        version: Sequence[int] | None = None,
+        tags: Mapping[str, str] | None = None,
+        sort_keys: bool = True,
+    ) -> None: ...
+
+class CDumper(CEmitter, SafeRepresenter, Resolver):
+    def __init__(
+        self,
+        stream: IO[Any],
+        default_style: str | None = None,
+        default_flow_style: bool = False,
+        canonical=None,
+        indent: int | None = None,
+        width: int | None = None,
+        allow_unicode=None,
+        line_break: str | None = None,
+        encoding: str | None = None,
+        explicit_start=None,
+        explicit_end=None,
+        version: Sequence[int] | None = None,
+        tags: Mapping[str, str] | None = None,
+        sort_keys: bool = True,
+    ) -> None: ...
+
+CSafeDumper = CDumper
---
+++ typings/yaml/serializer.pyi
@@ -0,0 +1,35 @@
+from typing import ClassVar
+
+from _typeshed import Incomplete
+from yaml.error import YAMLError
+from yaml.nodes import Node
+
+class SerializerError(YAMLError): ...
+
+class Serializer:
+    ANCHOR_TEMPLATE: ClassVar[str]
+    use_encoding: Incomplete
+    use_explicit_start: Incomplete
+    use_explicit_end: Incomplete
+    use_version: Incomplete
+    use_tags: Incomplete
+    serialized_nodes: dict[Incomplete, Incomplete]
+    anchors: dict[Incomplete, Incomplete]
+    last_anchor_id: int
+    closed: bool | None
+    def __init__(
+        self,
+        encoding=None,
+        explicit_start=None,
+        explicit_end=None,
+        version=None,
+        tags=None,
+    ) -> None: ...
+    def open(self) -> None: ...
+    def close(self) -> None: ...
+    def serialize(self, node: Node) -> None: ...
+    def anchor_node(self, node) -> None: ...
+    def generate_anchor(self, node) -> str: ...
+    def serialize_node(self, node, parent, index) -> None: ...
+
+__all__ = ["Serializer", "SerializerError"]
